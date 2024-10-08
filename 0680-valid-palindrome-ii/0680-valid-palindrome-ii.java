47c79365523135561f516be78021111ee7c73594
Prateek Mishra <126672828+Prats222@users.noreply.github.com>
2024-10-08 17:46:00 +0530

    Time: 4 ms (99.4%), Space: 45.5 MB (28.39%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+              return isPali(s,left+1,right)  || isPali(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    private boolean isPali(String s , int left , int right){
+    
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
