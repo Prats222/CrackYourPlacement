35e558577de6c2ee0a232398d4b36913d6326f42
Prateek Mishra <126672828+Prats222@users.noreply.github.com>
2024-11-01 11:50:45 +0530

    Time: 25 ms (100%), Space: 45.9 MB (21.25%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,27 +1,18 @@
 class Solution {
     public String makeFancyString(String s) {
-        char st = 'a';
-        char end = 'z';
-        char[] valid = new char[26];
-        
-        // Initialize valid array with characters from 'a' to 'z'
-        for (int i = 0; i < 26; i++) {
-            valid[i] = (char)(st + i);
-        }
-        
+        int sameCount = 0;
         StringBuilder sb = new StringBuilder();
-        int count = 0; // Counter for consecutive characters
-        
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            
-            // Check if current character is valid and matches the required conditions
-            if (sb.length() > 1 && sb.charAt(sb.length() - 1) == ch && sb.charAt(sb.length() - 2) == ch) {
-                continue; // Skip adding if it forms three consecutive characters
+        char prev = s.charAt(0);
+        for (char cur : s.toCharArray()) {
+            if (cur == prev) {
+                sameCount++;
             }
-            sb.append(ch);
+            else {
+                sameCount = 1;
+            }
+            if (sameCount < 3) sb.append(cur);
+            prev = cur;
         }
-        
         return sb.toString();
     }
 }
