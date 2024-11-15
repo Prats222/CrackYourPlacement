4d46c149549eaa74460a04117054bf2357f35c05
Prateek Mishra <126672828+Prats222@users.noreply.github.com>
2024-11-15 23:20:01 +0530

    Time: 2 ms (82.14%), Space: 64.8 MB (74.29%) - LeetHub

---
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findLengthOfShortestSubarray(int[] arr) {
+        int n = arr.length, left = 0, right = n - 1;
+
+        // Find the longest non-decreasing prefix
+        while (left < n - 1 && arr[left] <= arr[left + 1]) left++;
+
+        // If the array is already sorted
+        if (left == n - 1) return 0;
+
+        // Find the longest non-decreasing suffix
+        while (right > 0 && arr[right - 1] <= arr[right]) right--;
+
+        // Minimum of removing the prefix or suffix
+        int result = Math.min(n - left - 1, right);
+
+        // Check merging prefix and suffix
+        for (int i = 0; i <= left; i++) {
+            if (arr[i] <= arr[right]) {
+                result = Math.min(result, right - i - 1);
+            } else if (right < n - 1) {
+                right++;
+            }
+        }
+        return result;
+    }
+}
