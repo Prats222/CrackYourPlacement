8c1a2a2bc2f4152ce814cdf420f8b537f2c05ea8
Prateek Mishra <126672828+Prats222@users.noreply.github.com>
2024-12-21 12:00:10 +0530

    Time: 0 ms (100%), Space: 45.9 MB (7.48%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,21 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        // int n=nums.length;
+        // int freq[] = new int[n+1];
+        // for(int i=0;i<n;i++){
+        //     freq[nums[i]]++;
+        // }
+        // for(int i=0;i<freq.length;i++){
+        //     if(freq[i]==0){
+        //         return i;
+        //     }
+        // }
+        // return 0;
         int n=nums.length;
-        int freq[] = new int[n+1];
-        for(int i=0;i<n;i++){
-            freq[nums[i]]++;
+        int sum = n*(n+1)/2;
+        for(int num:nums){
+            sum-=num;
         }
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]==0){
-                return i;
-            }
-        }
-        return 0;
+        return sum;
     }
 }
