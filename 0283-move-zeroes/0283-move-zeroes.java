1d3433cc00aaff7207afeef172c7cf7de39c3484
Prateek Mishra <126672828+Prats222@users.noreply.github.com>
2024-12-21 11:24:20 +0530

    Time: 2 ms (84.87%), Space: 46.3 MB (6.05%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // all zeroes in a dif arr, all non-zero in dif ary, and combine. # not space effecient as 2 arr 
+        int[] zero = new int[nums.length];
+        int nonz[]= new int[nums.length];
+
+        int zeroI = 0; int nonzI=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                zero[zeroI++]=nums[i];
+            }else{
+                nonz[nonzI++]=nums[i];
+            }
+        }
+        int ind = 0;
+        for(int i=0;i<nonzI;i++){
+            nums[ind++]=nonz[i];
+        }
+        for(int i=0;i<zeroI;i++){
+            nums[ind++]=zero[i];
+        }
+    }
+}
